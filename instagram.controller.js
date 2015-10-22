0c43c2461d2b497ad53dd3212b4f5ffb65c428bb
Joseph <jchan@media.ucla.edu>
2015-10-21 23:57:09 -0400

    init commit

---
+++ instagram.controller.js
@@ -0,0 +1,43 @@
+angular.module('controller', [])
+
+//controller setup
+.controller('igCtrl', function($scope, Instagram){ //pass in scope
+  var id = '8e8390095f1f4b5c82a187442cc5bacc';
+  var rdi = 'http://ig.bruinmobile.com';
+  $scope.auth0='https://instagram.com/oauth/authorize/?client_id='+ id +'&redirect_uri='+ rdi +'&response_type=token';
+  $scope.loggedin=function(){
+    console.log('loggedin');
+  }
+	var currentURL = window.location.href;
+  if(!currentURL.match("access_token"))return; // add return to make run only if access token is available
+  var at = currentURL.slice(currentURL.indexOf('access_token'));
+	console.log(at);
+
+}) // dont need semi colon after ctrl
+
+/* .factory('igService', function($http){      //pass in http to function to use it
+  $http.get(url, function(data){               //http.get allows for http get request
+
+  })
+})
+*/
+.factory('Instagram', ['$http',
+	function($http) {
+		var base = "https://api.instagram.com/v1";
+		var clientId = '8e8390095f1f4b5c82a187442cc5bacc';
+		return {
+			'get': function(count, hashtag) {
+				var request = '/tags/' + hashtag + '/media/recent';
+				var url = base + request;
+				var config = {
+					'params': {
+						'client_id': clientId,
+						'count': count,
+						'callback': 'JSON_CALLBACK'
+					}
+				};
+				return $http.jsonp(url, config); // gets json file from the url its passed to
+			}
+		};
+	}
+]);
