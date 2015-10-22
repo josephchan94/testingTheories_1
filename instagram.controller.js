5e142c363a8608f92d646e854d58102759a92308
josephchan94 <jchan@media.ucla.edu>
2015-10-21 21:34:38 -0700

    working

--- instagram.controller.js
+++ instagram.controller.js
@@ -5,17 +5,15 @@ angular.module('controller', [])
   var id = '8e8390095f1f4b5c82a187442cc5bacc';
   var rdi = 'http://ig.bruinmobile.com';
   $scope.auth0='https://instagram.com/oauth/authorize/?client_id='+ id +'&redirect_uri='+ rdi +'&response_type=token';
-  $scope.loggedin=function(){
+  $scope.loggedin=function(){ //this function is called when you click button in index.html with ng-click
     console.log('loggedin');
   }
-	var currentURL = window.location.href;
-  if(!currentURL.match("access_token"))return; // add return to make run only if access token is available
-  var at = currentURL.slice(currentURL.indexOf('access_token'));
+	var currentURL = window.location.href; //grabs the current url and puts it in var currentURL
+  if(!currentURL.match("access_token"))return; // if no access token in currentURL, do nothing
+  var at = currentURL.slice(currentURL.indexOf('access_token')); // slices currentURL to get active token
 	console.log(at);
 
-
-
-  Instagram.get(9, "bunny").success(function(response) {
+  Instagram.get(9, "bunny").success(function(response) { //Instagram factory init. .get is a property of Instagram factory, it's a function. It gets the number of images posted back, and the hashtag.
     $scope.images=response.data; //IG function returns a promise, when promise is .success, , returns function as parameter
     console.log($scope.images);
   });
@@ -48,3 +46,7 @@ angular.module('controller', [])
 		};
 	}
 ]);
+
+// <input type = "text" ng-model = "variablename">
+// variable can accessed to a $scope.variablename
+// add a button to do refresh & randomizer.
